0993e3bd4efc05416bfc40e067b76ed862673cf8
Alfin123-stack <alfinkarin789@gmail.com>
2026-05-30 13:26:00 +0700

    fix supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,13 @@
 // lib/supabase/client.ts
 import { createBrowserClient } from "@supabase/ssr";
 
-export const supabase = createBrowserClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!,
-);
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error(
+    "Environment variable NEXT_PUBLIC_SUPABASE_URL dan NEXT_PUBLIC_SUPABASE_ANON_KEY wajib diisi.",
+  );
+}
+
+export const supabase = createBrowserClient(supabaseUrl, supabaseAnonKey);
